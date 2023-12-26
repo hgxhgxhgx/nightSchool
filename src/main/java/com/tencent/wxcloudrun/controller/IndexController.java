6fb5d821eea9a0ca4514ee0f4231e9dd79575002
hgx <changfeng@huguoxingdeMacBook-Pro.local>
2023-12-26 16:53:44 +0800

    v1  增加获取课程接口
    Signed-off-by: hgx <changfeng@huguoxingdeMacBook-Pro.local>

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -1,22 +1,41 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.tencent.wxcloudrun.config.ApiResponse;
+import com.tencent.wxcloudrun.model.bizDO.CourseInfoDO;
+import com.tencent.wxcloudrun.service.CourseInfoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * index控制器
  */
-@Controller
-
+@RestController
+@RequestMapping("/index")
+@Slf4j
 public class IndexController {
+
+  @Autowired
+  CourseInfoService courseInfoService;
 
   /**
    * 主页页面
    * @return API response html
    */
-  @GetMapping
-  public String index() {
-    return "index";
+  @GetMapping("/getRecommendCourse")
+  public ApiResponse getRecommendCourse() {
+    log.info("getRecommendCourse in");
+    List<CourseInfoDO> limitCourse = courseInfoService.getLimitCourse(10);
+    log.info("getRecommendCourse out");
+    return ApiResponse.ok(limitCourse);
+
   }
 
 }
