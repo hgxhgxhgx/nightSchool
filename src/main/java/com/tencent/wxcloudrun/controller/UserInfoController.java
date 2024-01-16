64a58def1eac4f4c48729525062250dc60d99c88
hgx <279973155@qq.com>
2024-01-16 18:59:13 +0800

    v1  增加获取用户信息接口
    Signed-off-by: hgx <279973155@qq.com>

--- src/main/java/com/tencent/wxcloudrun/controller/UserInfoController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/UserInfoController.java
@@ -1,7 +1,7 @@
 package com.tencent.wxcloudrun.controller;
 
 
-import com.sun.deploy.net.HttpUtils;
+
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.service.UserInfoService;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.net.ssl.HttpsURLConnection;
 
 @RestController
 @Slf4j
